0154744be3d16762802dddb03499dfa4372a12ec
William <suttonwilliamd@gmail.com>
2025-04-16 03:45:24 -0400

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,8 @@
-// src/pages/rss.xml.js
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  // Assuming you'll eventually store posts in Astro's content collection
-  // For now, we can use the posts.json file
+  
   const posts = await fetch(new URL('/posts.json', context.site)).then(res => res.json());
   
   return rss({
