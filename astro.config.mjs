0351f9fd906ce921b5fdf79bbf9eb7d0bac4a5a5
William <suttonwilliamd@gmail.com>
2025-04-16 02:36:30 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,21 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://william64.com',
+  integrations: [
+    react(),
+    tailwind(),
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'one-dark-pro' }
+    }),
+    sitemap()
+  ],
+  markdown: {
+    rehypePlugins: ['rehype-slug', 'rehype-autolink-headings']
+  }
 });
